4a81a9cbc419efe18f27fae616878f9a71cfb54e
sankaralingam <125450819+msankaralingam@users.noreply.github.com>
2023-06-23 10:27:47 +0530

    Update main.js

--- main.js
+++ main.js
@@ -14,3 +14,30 @@ var a=prompt("enter the number");
 	console.log(i);	
 }
 }
+
+// define prime number
+var a =prompt("Enter a positive number");
+var b = 0;
+
+ if (a>1) {
+
+
+    for (let i = 2; i <=a; i++) {
+        if (a % i == 0) {
+            
+            b++;
+        }
+    }
+
+    if (b==1) {
+        console.log(" prime number");
+    } 
+	else {
+        console.log("not prime number");
+    }
+}
+
+
+else {
+    console.log("composite number.");
+}
